12fa0a02b39bfb863199ffa03568c475a270e1bb
= <=>
2023-09-26 00:55:50 +0300

    Complete mega menu

---
+++ src/components/childrenMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="childs in childrens" :key="childs.id">
+      <router-link :to="'/' + childs.id">
+        <p>{{ childs.name }}</p>
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['childrens']
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
